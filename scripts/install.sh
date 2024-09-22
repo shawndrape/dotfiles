2708971409e30ce8a5fc55e9e10b6b5ca6a5e04c
Shawn Drape <shawndrape@users.noreply.github.com>
2024-09-21 23:22:00 -0400

    Initial version of install script

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+# dependency check
+if ! command -v brew &> /dev/null; 
+  then echo "Homebrew is not installed. Please install it first.";
+   exit 1;
+fi
+
+# pre-install
+if ! brew ls --versions "stow"; then brew install "stow"; fi
+
+# Main install
+stow .
+
+# setup iterm2
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
