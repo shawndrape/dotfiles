bbe12d05167618f3f2fcd3a24aacf9ccea24b490
Shawn Drape <shawndrape@users.noreply.github.com>
2024-09-21 23:44:07 -0400

    Correct iterm profile name

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,6 +12,6 @@ stow .
 
 # setup iterm2
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2_profile"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
